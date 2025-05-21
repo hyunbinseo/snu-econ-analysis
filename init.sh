49f3c28cefcdd3995f969b00d8dd0d26f0d7fcc4
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-05-21 15:06:16 +0900

    chore: install pmdarima

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-uv init
+uv init --python 3.12 # https://github.com/alkaline-ml/pmdarima/issues/588
+
 uv run --with jupyter jupyter lab
 
 # Within a notebook, you can import your project's modules as you would in any other file in the project. For example, if your project depends on requests, import requests will import requests from the project's virtual environment.
